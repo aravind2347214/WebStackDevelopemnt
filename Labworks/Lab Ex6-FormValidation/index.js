74acd2bd0dc0e436cee6603c655d31492b779f20
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-09-01 13:21:30 +0530

    completed the lab ex 6 with the live validation

--- Labworks/Lab Ex6-FormValidation/index.js
+++ Labworks/Lab Ex6-FormValidation/index.js
@@ -1,74 +1,19 @@
-let flag=null;
+let nameflag=null;
+let passwordflag=null;
+let dobflag=null;
+let cpasswordflag=null;
+let emailflag=null;
 
 const submitHandler =(e)=>{
-    const name= document.getElementById("name").value;
-    const email= document.getElementById("email").value;
-    const dob= document.getElementById("dob").value;
-    const password= document.getElementById("password").value;
-    const cpassword= document.getElementById("c_password").value;
-
-
-    const dobRegex=/^([0-9]{2})-([0-9]{2})-([0-9]{4})$/ 
-    if(dob===null || dob==="" || dobRegex.test(dob)|| new Date(dob)>new Date||(getAge(dob)<18)){
-        document.getElementById("dob-error").innerText=`${getAge(dob)<18?"Under 18":"Invalid Age"}`
-        document.getElementById("dob-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
-        flag=false
-        setTimeout(() => {
-            document.getElementById("dob-error").innerText=""
-            document.getElementById("dob-error").style.backgroundColor="transparent"
-        },5000);   
-    }
-    else{
-        flag=true;
-    }
-    
-    
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if(!emailRegex.test(email)){
-        document.getElementById("email-error").innerText="Invalid Email"
-        document.getElementById("email-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
-        flag=false
-        setTimeout(() => {
-            document.getElementById("email-error").innerText=""
-            document.getElementById("email-error").style.backgroundColor="transparent"
-        },5000);
-    }
-    else{
-        flag=true;
-    }
-  
-
-    
-
-    const nameRegex= /^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/;
-    // console.log(nameRegex.test(name));
-    if(name===""|| name===" " ||name===null){
-        flag=false
-        document.getElementById("name-error").innerText="Name cannot be empty"
-        document.getElementById("name-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
-        setTimeout(() => {
-            document.getElementById("name-error").innerText=""
-            document.getElementById("name-error").style.backgroundColor="transparent"
-        },5000);
-
-    }
-    else if(!nameRegex.test(name)){
-        flag=false
-        document.getElementById("name-error").innerText="Only alphabets and spaces are allowed"
-        document.getElementById("name-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
-        setTimeout(() => {
-            document.getElementById("name-error").innerText=""
-            document.getElementById("name-error").style.backgroundColor="transparent"
-        },5000);
-
-    }
-    else{
-        flag=true
-    }
-
+    nameValidate();
+    emailValidate();
+    dobValidate();
+    passwordValidate();
+    const password=document.getElementById("password").value;
+    const cpassword=document.getElementById("c_password").value;
     const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/
     if(passwordRegex.test(password)){
-        flag=false
+        passwordflag=true
         if(!(password===cpassword)){
         document.getElementById("cpass-error").innerText="Password does not match"
         document.getElementById("cpass-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
@@ -78,20 +23,15 @@ const submitHandler =(e)=>{
         },5000);
         }
         else{
-            flag=true
+            cpasswordflag=true
         }
     }
-    else if(!passwordRegex.test(password)){
-        flag=false
-        document.getElementById("pass-error").innerText="1 Digit 1 ,Uppercase 1 ,Lowercase 1 ,Special Charecter and of length 8"
-        document.getElementById("pass-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
-        setTimeout(() => {
-            document.getElementById("pass-error").innerText=""
-            document.getElementById("pass-error").style.backgroundColor="transparent"
-        },5000);
-        }
-        // console.log(flag);
-        if(flag===true && flag!=null){
+
+
+   
+
+        // validation Sucesss;
+        if(cpasswordflag===true && passwordflag ===true&& nameflag===true && emailflag===true && dobflag===true){
             document.getElementById("success").style.display="block"
             setTimeout(() => {
                 document.getElementById("success").style.display="none"
@@ -127,4 +67,105 @@ const getAge=(dob)=>{
     }    
     return age;
 
+}
+
+
+
+
+const nameValidate =(e)=>{
+    const name= document.getElementById("name").value;
+    const nameRegex= /^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/;
+    if(name===""|| name===" " ||name===null){
+        nameflag=false
+        document.getElementById("name-error").innerText="Name cannot be empty"
+        document.getElementById("name-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
+        setTimeout(() => {
+            document.getElementById("name-error").innerText=""
+            document.getElementById("name-error").style.backgroundColor="transparent"
+        },5000)
+
+    }
+    else if(!nameRegex.test(name)){
+        nameflag=false
+        document.getElementById("name-error").innerText="Only alphabets and spaces are allowed"
+        document.getElementById("name-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
+        setTimeout(() => {
+            document.getElementById("name-error").innerText=""
+            document.getElementById("name-error").style.backgroundColor="transparent"
+        },5000);
+
+    }
+    else{
+        nameflag=true
+    }    
+}
+
+const emailValidate =()=>{
+    const email= document.getElementById("email").value;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if(!emailRegex.test(email)){
+        document.getElementById("email-error").innerText="Invalid Email"
+        document.getElementById("email-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
+        emailflag=false
+        setTimeout(() => {
+            document.getElementById("email-error").innerText=""
+            document.getElementById("email-error").style.backgroundColor="transparent"
+        },5000);
+    }
+    else{
+        emailflag=true;
+    }   
+}
+
+const dobValidate =(e)=>{
+    const dob= document.getElementById("dob").value;
+    const dobRegex=/^([0-9]{2})-([0-9]{2})-([0-9]{4})$/ 
+    if(dob===null || dob==="" || dobRegex.test(dob)|| new Date(dob)>new Date||(getAge(dob)<18)){
+        document.getElementById("dob-error").innerText=`${getAge(dob)<18?"Under 18":"Invalid Age"}`
+        document.getElementById("dob-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
+        dobflag=false
+        setTimeout(() => {
+            document.getElementById("dob-error").innerText=""
+            document.getElementById("dob-error").style.backgroundColor="transparent"
+        },5000);   
+    }
+    else{
+        dobflag=true;
+    }
+    
+}
+
+const passwordValidate =(e)=>{
+    const password= document.getElementById("password").value;
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/
+    if(passwordRegex.test(password)){
+        passwordflag=true
+    }
+    else if(!passwordRegex.test(password)){
+        passwordflag=false
+        document.getElementById("pass-error").innerText="1 Digit 1 ,Uppercase 1 ,Lowercase 1 ,Special Charecter and of length 8"
+        document.getElementById("pass-error").style.backgroundColor="rgba(255, 169, 169, 0.2)"
+        setTimeout(() => {
+            document.getElementById("pass-error").innerText=""
+            document.getElementById("pass-error").style.backgroundColor="transparent"
+        },5000);
+        } 
+}
+
+
+const cpasswordValidate =(e)=>{
+    
+}
+
+
+const showPassword=()=>{
+    document.getElementById("password").type="text"
+    document.getElementById("closeeye").style.display="none";
+    document.getElementById("openeye").style.display="block";
+}
+
+const hidePassword=()=>{
+    document.getElementById("password").type="password"
+    document.getElementById("openeye").style.display="none";
+    document.getElementById("closeeye").style.display="block";
 }
