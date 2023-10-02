19ad90a2ce1a4b68fd92bc2d0cb1d8952912c5f0
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-10-02 14:32:14 +0530

    Lab 10 completed

--- Labworks/Lab Ex10-MovieApp/movieapp/src/pages/ContactPage.jsx
+++ Labworks/Lab Ex10-MovieApp/movieapp/src/pages/ContactPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <>
+    <Navbar activeTab="contact"/>
+    </>
   )
 }
 
