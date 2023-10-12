0372e0ef57c559b68819de11d16494d43b855266
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-10-12 22:06:38 +0530

    lab 11 complete

--- Labworks/Lab Ex13-CRUDapp/frontend/src/Component.jsx
+++ Labworks/Lab Ex13-CRUDapp/frontend/src/Component.jsx
@@ -4,6 +4,11 @@ import React, { useEffect, useState } from 'react'
 function Component() {
 
     const [notes,setNotes]=useState([])
+    const [showAddNote,setshowAddNote]=useState(false)
+    const [formData,setFormData]=useState({
+        title:'',
+        content:''
+    })
 
     useEffect(()=>{
         if(notes.length===0){
@@ -16,13 +21,50 @@ function Component() {
         }
     },[])
 
+    const handleAddNote=()=>{
+        setshowAddNote(true)
+
+
+    }
+
+    const handleAddCancel=()=>{
+        setshowAddNote(false)
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        const data={
+            title:formData.title,
+            content:formData.content,
+            timestamp:new Date.now()
+        }
+        axios.post("http://localhost:8000/add-note",data).then((res)=>{
+            alert('Note added successfully')
+            setFormData({ 
+            title:'',
+            content:''
+          })
+          setshowAddNote(false)
+
+        }).catch((err)=>{
+            console.log(err);
+            alert("Could Not Add note")
+        })
+
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+      
     
 
 
 
 
   return (
-    <div className='w-[60%] bg-slate-100 mx-auto p-2 mt-20 rounded-md flex flex-col gap-2'>
+    <div className='w-[50%] bg-slate-100 mx-auto p-2 mt-20 rounded-md flex flex-col gap-2'>
         <div className='text-[50px] text-center font-extrabold'>Notes App</div>
 
 
@@ -46,9 +88,41 @@ function Component() {
                 </tbody>
             </table>
         </div>
-        {/* <div className='flex flex-row justify-between'>
-            <button onClick={handleAddNote}>Add Note</button>
-        </div> */}
+        <div className='flex flex-row flex-end w-full justify-end  items-center my-2'>
+            <button className='px-3 py-1 text-black bg-[#01010158]  hover:bg-[#010101] rounded-md hover:text-white font-bold '  onClick={handleAddNote}>+ Add Note</button>
+        </div>
+
+        {
+            showAddNote&&
+            <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+                <div className='flex flex-col'>
+                    <label htmlFor="">Note Title</label>
+                    <input
+                    name="title"
+                    onChange={handleInputChange} 
+                    
+                    type="text" className='p-2' placeholder='Enter a Title' required/>
+                </div>
+
+                <div className='flex flex-col'>
+                    <label htmlFor="">Note Content</label>
+                    <input
+                    name="content"
+                    onChange={handleInputChange}
+                    type="text" className='p-2' placeholder='Enter Some Content' required/>
+                </div>
+                <div className='flex flex-row gap-2 justify-end'>
+
+                <button type='submit' className=' border-black w-fit px-3 py-1 text-black hover:bg-[#01010114]   rounded-md  font-bold ' onClick={handleAddCancel}>
+                    Cancel
+                </button>
+                <button type='submit' className=' w-fit px-6 py-1 text-white bg-[#010101]   rounded-md  font-bold ' >
+                    Add
+                </button>
+                </div>
+                
+            </form>
+        }
 
 
     </div>
